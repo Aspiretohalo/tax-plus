d7147f060f40cb140a086fbfdec45a5d995d01fa
lilboydontkill <124600268+lilboydontkill@users.noreply.github.com>
2023-10-21 14:14:40 +0800

    个人信息
    个人信息

---
+++ src/components/LeftMenuSMsg.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="leftCard">
+        <el-menu :router="true" default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+
+            <div class="touxiang">
+                <span class="kong"></span>
+                <el-avatar :size="50" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
+                <span class="kong"></span>
+                <el-text class="name">zjt</el-text>
+            </div>
+            
+
+
+
+
+            <el-menu-item :index="item.router" class="el-menu-item" v-for="item in menuDemo2">
+                <el-icon class="icon">
+                    <!-- <SvgIcon :className="item.icon"></SvgIcon> -->
+                    <component :is="item.icon"></component>
+                </el-icon>
+                <span>{{ item.name }}</span>
+            </el-menu-item>
+        </el-menu>
+
+    </div>
+</template>
+  
+<script lang="ts" setup>
+import { reactive } from 'vue'
+// import { useRouter } from 'vue-router'
+
+// const router = useRouter()
+const menuDemo2 = reactive([
+    { name: '个人信息', index: 1, icon: 'User', router: '/StudentMsg' },
+    { name: '评论区', index: 3, icon: 'ChatDotRound', router: '/community' },
+    { name: '返回主页', index:4, icon:'House', router:'/courseManage'}
+])
+
+const handleOpen = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+}
+
+</script>
+  
+<style scoped>
+.demo-type {
+    display: flex;
+}
+
+.demo-type>div {
+    flex: 1;
+    text-align: center;
+}
+
+.demo-type>div:not(:last-child) {
+    border-right: 1px solid var(--el-border-color);
+}
+
+.leftCard {
+    position: fixed;
+    background-color: #f9f9f9;
+    height: 100%;
+}
+
+.el-menu-vertical-demo {
+    width: 258px;
+}
+
+.el-menu-item span {
+    padding-left: 0px;
+}
+
+.el-menu-item .icon {
+    margin-left: 20px;
+}
+
+.name {
+    
+    padding-top: 35px;
+    padding-bottom: 35px;
+    text-align: center;
+}
+
+.touxiang {
+    display: flex;
+    align-items: center;
+    /* justify-content: center; */
+}
+
+.kong {
+    width:25px;
+}
+::v-deep .el-menu {
+    /* margin-top: 100px; */
+    border-right: 0;
+    background-color: #f9f9f9;
+}
+
+::v-deep .el-card {
+    position: absolute;
+    bottom: 50px;
+    left: 20px;
+    width: 160px;
+    height: 211px;
+    border-radius: 10px;
+}
+
+/* ::v-deep .el-avatar {
+    background-color: #0089ff;
+} */
+</style>
--- src/pages/student/StudentMsg.vue
+++ src/pages/student/StudentMsg.vue
@@ -1,9 +1,140 @@
 <template>
-  <div>StudentMsg</div>
+  <div>
+    <el-container style="display:flex">
+      <el-aside>
+        <LeftMenuSMsg></LeftMenuSMsg>
+      </el-aside>
+      <el-main class="main">
+        <div style="width:100%">
+          <div class="Msgbackground">
+            <img src="src\assets\imgs\Msgbackground.png" alt="cover">
+          </div>
+          <div class="bgavatar">
+            <div class="imgbox">
+              <img class="imgtip" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png">
+            </div>    
+          </div>
+          <div>
+            <span class="bgname">证件套</span>
+          </div>
+
+          <el-col :span="6">
+            
+            <el-row :gutter="12" style="margin-top:30px;">
+              <el-col :span="8">
+                <div style="text-align:right; color: gray;"><span>手机号：</span></div>
+              </el-col>
+              <el-col :span="16">17125463265</el-col>
+            </el-row>
+            <el-row :gutter="12" style="margin-top:30px;">
+              <el-col :span="8">
+                <div style="text-align:right; color: gray;"><span>电子邮箱：</span></div>
+              </el-col>
+              <el-col :span="16">2036128127@qq.com</el-col>
+            </el-row>
+            <el-row :gutter="12" style="margin-top:30px;">
+              <el-col :span="8">
+                <div style="text-align:right; color: gray;"><span>部门：</span></div>
+              </el-col>
+              <el-col :span="16">某某管理中心</el-col>
+            </el-row>
+            <el-row :gutter="12" style="margin-top:30px;">
+              <el-col :span="8">
+                <div style="text-align:right; color: gray;"><span>身份：</span></div>
+              </el-col>
+              <el-col :span="16">学生</el-col>
+            </el-row>
+            <!-- <el-row :gutter="12" style="margin-top:30px;">
+              <el-col :span="8">
+                <div style="text-align:right; color: gray;"><span>个人介绍：</span></div>
+              </el-col>
+              <el-col :span="16">对方很懒没留下什么</el-col>
+            </el-row> -->
+          </el-col>
+
+        </div>
+
+
+      </el-main>
+
+    </el-container>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import LeftMenuSMsg from '../../components/LeftMenuSMsg.vue'
+
 </script>
 
 <style lang="scss" scoped>
-</style>
+body {
+  background-color: #fff;
+}
+
+.el-descriptions {
+  margin-top: 20px;
+}
+
+::v-deep .el-main {
+  --el-main-padding: 0px background-color: #f9f9f9;
+}
+
+::v-deep .el-col-6 {
+  padding-left: 80px;
+}
+
+
+.Msgbackground {
+  flex: 1;
+  position: relative;
+
+  width: 100%;
+  max-height: 320px;
+  object-fit: cover;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+.Msgbackground img {
+  z-index: 1;
+  width: 100%;
+  height: fit-content;
+}
+
+
+
+.bgname{
+  padding-left: 85px;
+  font-size: 45px;
+  font-weight: 300;
+  font-family: 'Hiragino Sans GB';
+
+}
+
+.imgbox {
+  padding-left: 120px;
+  z-index: 20;
+  flex: 1;
+  position: relative;
+  height: 112px;
+  width: 112px;
+  align-items: center;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+
+
+}
+
+.imgtip {
+
+  transform: translateY(-50%);
+  border-radius: 50%;
+  height: 100%;
+  width: 100%;
+
+}</style>
