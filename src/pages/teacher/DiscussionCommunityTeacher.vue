6b43ee7003b9a29a0574893847485998a0facea6
lilboydontkill <124600268+lilboydontkill@users.noreply.github.com>
2023-10-28 23:11:52 +0800

    小改不算改
    小改不算改

--- src/pages/teacher/DiscussionCommunityTeacher.vue
+++ src/pages/teacher/DiscussionCommunityTeacher.vue
@@ -7,7 +7,7 @@
       <el-main>
         <el-card class="box-card welcomeCard">
           <span class="welcome">讨论区</span>
-          <el-button type="warning" class="goOnLearning" @click="dialogFormVisible = true">创建讨论</el-button>
+          <el-button type="primary" class="goOnLearning" @click="dialogFormVisible = true">创建讨论</el-button>
           <el-dialog v-model="dialogFormVisible" title="我的讨论">
             <el-form :model="form">
               <el-form-item label="讨论内容">
@@ -34,6 +34,10 @@
           </el-dialog>
         </el-card>
         <el-card class="box-card notice">
+          <div style="display: flex;justify-content: right;padding-right: 30px;">
+            <el-input v-model="input" placeholder="请输入" clearable />
+            <el-button type="primary" :icon="Search" circle />
+          </div>
           <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
             <el-tab-pane label="最新" name="first">
               <el-card class="box-card-small">
@@ -49,21 +53,19 @@
                     <el-image style="width: 100px; height: 100px" :src="url" :zoom-rate="1.2" :max-scale="7"
                       :min-scale="0.2" :preview-src-list="srcList" :initial-index="4" fit="cover" />
                   </div>
-
-
                 </div>
 
+
+
+
               </el-card>
-              <div><el-pagination background layout="prev, pager, next" :total="1000" /></div>
+              
 
             </el-tab-pane>
             <el-tab-pane label="全部" name="second">全部</el-tab-pane>
             <el-tab-pane label="我的" name="third">我的</el-tab-pane>
           </el-tabs>
-
         </el-card>
-
-
 
       </el-main>
     </el-container>
@@ -75,9 +77,12 @@ import LeftMenu from '../../components/LeftMenu.vue'
 import { reactive, ref } from 'vue'
 import type { TabsPaneContext } from 'element-plus'
 import { ElMessage, ElMessageBox } from 'element-plus'
-
+import {
+  Search,
+} from '@element-plus/icons-vue'
 import type { UploadProps, UploadUserFile } from 'element-plus'
 
+const input = ref('')
 const fileList = ref<UploadUserFile[]>([
   {
     name: 'element-plus-logo.svg',
@@ -148,9 +153,17 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
 </script>
   
 <style  scoped>
-div {
-  display: inline-block;
+.example-pagination-block+.example-pagination-block {
+  margin-top: 10px;
 }
+
+.example-pagination-block .example-demonstration {
+  margin-bottom: 16px;
+}
+
+/* div {
+  display: inline-block;
+} */
 
 h1,
 h2,
